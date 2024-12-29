1b6a8c9929c8e286301eb70a12916647a0cc0152
Nestor Fabio Gomez Toro <neyblue15@hotmail.com>
2024-12-29 00:06:34 -0500

    clean up screens

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, StyleSheet } from "react-native";
+
+
+
+export default function MainScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  }
+
+})
